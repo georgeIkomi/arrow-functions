d250cbba02c9819f62efccd6fb94b0676d4ef5e0
Eyituoyo George Ikomi <ikomieyituoyogeorge@yahoo.com>
2023-05-16 10:01:36 +0000

    Create arrow-functions.js for practice

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,39 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+
+let sum = addTwoNumbers(3, 5)
+console.log(sum);
 
 // Arrow Function With Parameters
+const addTwoNumbers2 = (a, b) => {
+    return a + b;
+}
+
+let sum2 = addTwoNumbers2(6, 4)
+console.log(sum2);
+
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers3 = (a, b) => a + b
+let sum3 = addTwoNumbers(2, 10);
+console.log(sum3)
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething("Hello there!!");
+
+const sayHello = () => console.log("Hello");
+sayHello()
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiline string!
+  </p>`
+)
+
+console.log(returnMultipleLines());
